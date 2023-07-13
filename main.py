585bd131ab9052fd705d404f590f5173c380a10c
Ded Wildfield <wildfielded@wildfielded.site>
2023-07-13 15:34:57 +0800

    Workday save4 13-07-2023 	modified:   .gitignore         - Some adds 	modified:   README.md         - New thoughts 	modified:   main.py         - SQLAlchemy introduced 	new file:   sqlite/sqlite3.def 	new file:   sqlite/sqlite3.dll 	new file:   sqlite/sqlite3.exe

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+import json
+import uuid
+
 from bottle import HTTPError, get, post, request, run
+import sqlalchemy as sa
+from sqlalchemy.orm import declarative_base, Session
 
 
 ''' =====----- Global variables -----===== '''
@@ -19,6 +24,18 @@ def server_root() -> str:
     '''
     with open(ROOT_INDEX_FILE, 'r', encoding='utf-8') as f_:
         return f_.read()
+
+
+@post('/srv1/auth/login')
+def login_post(credentials_: dict) -> dict:
+    ''' Ресурс аутентификации на сервере
+    Arguments:
+        credentials_ [dict] -- Словарь/json с ключами 'login',
+            'password'
+    Returns:
+        [dict] -- Словарь/json с токеном пользователя 'token',
+            или с ключами 'code' и 'text' в случае ошибки    '''
+    pass
 
 
 ''' =====----- MAIN -----===== '''
