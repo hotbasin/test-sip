f9f97d5463ff78383d75f838ba6ba062d22ffeaf
Ded Wildfield <wildfielded@wildfielded.site>
2023-07-26 23:44:35 +0800

    Diary3 26-07-2023

--- .SIPdev/call.sh
+++ .SIPdev/call.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/bash
 
+echousage() {
+    echo "Usage: ${0} \$1 \$2 \$3"
+    echo '    $1 - SIP-server IP/FQDN'
+    echo '    $1 - client login'
+    echo '    $1 - client passwd'
+    echo ''
+    exit 1
+}
+
+if [ "${1}" ] ; then
+    SERVER="${1}"
+    if [ "${2}" ] ; then
+        LOGIN="${2}"
+        if [ "${3}" ] ; then
+            PASSWORD="${3}"
+        else
+            echousage
+        fi
+    else
+        echousage
+    fi
+else
+    echousage
+fi
+
+PHONE='89021685218'
+SOUND='/home/ubuntu/SIPdev/sirena.wav'
 CONTROL='/usr/bin/linphonecsh'
 
 ${CONTROL} init
@@ -10,15 +37,15 @@ ${CONTROL} generic "soundcard use files"
 echo 'PHONE use WAV'
 sleep 1
 
-${CONTROL} register --username 398736 --host sip.novofon.com --password mH9V7x8yfm
+${CONTROL} register --username ${LOGIN} --host ${SERVER} --password ${PASSWORD}
 echo 'PHONE registered'
 sleep 1
 
-${CONTROL} dial sip:89021685218@sip.novofon.com
+${CONTROL} dial sip:${PHONE}@${SERVER}
 echo 'PHONE dialing'
 sleep 15
 
-${CONTROL} generic "play /home/stalk/DEVEL/sirena.wav"
+${CONTROL} generic "play ${SOUND}"
 echo 'PHONE playing'
 sleep 10
 
